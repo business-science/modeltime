089a9789e2e941e1b1e277b185312e9ffe2cc3e8
Matt Dancho <mdancho@gmail.com>
2021-03-20 19:12:55 -0400

    update doc

--- R/modeltime-recursive.R
+++ R/modeltime-recursive.R
@@ -164,29 +164,34 @@
 #'
 #'
 #' @export
-recursive <- function(object, transform, train_tail, ...){
+recursive <- function(object, transform, train_tail, id = NULL, ...){
     UseMethod("recursive")
 }
 
 #' @export
-recursive.model_fit <- function(object, transform, train_tail, ...) {
+recursive.model_fit <- function(object, transform, train_tail, id = NULL, ...) {
 
     dot_list <- list(...)
 
-    object$spec[["forecast"]]   <- "recursive"
-    object$spec[["transform"]]  <- .prepare_transform(transform)
+    .class_obj <- if(!is.null(id)){"recursive_panel"} else {"recursive"}
+
+    object$spec[["forecast"]]   <- .class_obj
+    object$spec[["transform"]]  <- if(!is.null(id)){.prepare_panel_transform(transform)} else {.prepare_transform(transform)}
     object$spec[["train_tail"]] <- train_tail
+    object$spec[["id"]] <- id
 
     # Workflow: Need to pass in the y_var
     object$spec[["y_var"]]      <- dot_list$y_var # Could be NULL or provided by workflow
 
     .class <- class(object)
-    class(object) <- c(.class[1], "recursive", .class[2])
+
+    class(object) <- c(.class[1], .class_obj, .class[2])
+
     object
 }
 
 #' @export
-recursive.workflow <- function(object, transform, train_tail, ...) {
+recursive.workflow <- function(object, transform, train_tail, id = NULL, ...) {
 
     # object$fit$fit$fit$spec[["forecast"]] <- "recursive"
     # object$fit$fit$fit$spec[["transform"]] <- .prepare_transform(transform)
@@ -195,14 +200,31 @@ recursive.workflow <- function(object, transform, train_tail, ...) {
     mld         <- object %>% workflows::pull_workflow_mold()
     y_var       <- names(mld$outcomes)
 
-    object$fit$fit <- recursive(
-        object     = object$fit$fit,
-        transform  = transform,
-        train_tail = train_tail,
-        y_var      = y_var
-    )
-    .class <- class(object)
-    class(object) <- c("recursive", .class)
+    if (is.null(id)){
+
+        object$fit$fit <- recursive(
+            object     = object$fit$fit,
+            transform  = transform,
+            train_tail = train_tail,
+            y_var      = y_var
+        )
+        .class <- class(object)
+        class(object) <- c("recursive", .class)
+    } else {
+
+        object$fit$fit <- recursive(
+            object     = object$fit$fit,
+            transform  = transform,
+            train_tail = train_tail,
+            y_var      = y_var,
+            id         = id
+        )
+        .class <- class(object)
+        class(object) <- c("recursive_panel", .class)
+
+    }
+
+
     object
 }
 
@@ -217,6 +239,21 @@ print.recursive <- function(x, ...) {
 
     y <- x
     class(y) <- class(y)[class(y) %>% stringr::str_detect("recursive", negate = TRUE)]
+    print(y)
+    invisible(x)
+}
+
+#' @export
+print.recursive_panel <- function(x, ...) {
+
+    if (inherits(x, "model_fit")) {
+        cat("Recursive [parsnip model]\n\n")
+    } else {
+        cat("Recursive [workflow]\n\n")
+    }
+
+    y <- x
+    class(y) <- class(y)[class(y) %>% stringr::str_detect("recursive_panel", negate = TRUE)]
     print(y)
     invisible(x)
 }
@@ -243,6 +280,34 @@ predict.recursive <- function(object, new_data, type = NULL, opts = list(), ...)
     if (inherits(object, "workflow")) {
         # print("Recursive Workflow")
         ret <- predict_recursive_workflow(object, new_data, type = NULL, opts = list(), ...)
+    }
+
+    return(ret)
+
+}
+
+#' Recursive Model Predictions
+#'
+#' Make predictions from a recursive model.
+#'
+#' @inheritParams parsnip::predict.model_fit
+#'
+#' @details
+#'
+#' Refer to [recursive_panel()] for further details and examples.
+#'
+#'
+#' @export
+predict.recursive_panel <- function(object, new_data, type = NULL, opts = list(), ...) {
+
+    if (inherits(object, "model_fit")) {
+        # print("Recursive Model fit")
+        ret <- predict_recursive_panel_model_fit(object, new_data, type = NULL, opts = list(), ...)
+    }
+
+    if (inherits(object, "workflow")) {
+        # print("Recursive Workflow")
+        ret <- predict_recursive_panel_workflow(object, new_data, type = NULL, opts = list(), ...)
     }
 
     return(ret)
@@ -299,9 +364,9 @@ predict_recursive_model_fit <- function(object, new_data, type = NULL, opts = li
         .nth_slice <- .transform(.temp_new_data, nrow(new_data), i)
 
         .preds[i,] <- new_data[i, y_var] <- pred_fun(
-                object, new_data = .nth_slice,
-                type = type, opts = opts, ...
-            )
+            object, new_data = .nth_slice,
+            type = type, opts = opts, ...
+        )
     }
 
     return(.preds)
@@ -324,6 +389,145 @@ predict_recursive_workflow <- function(object, new_data, type = NULL, opts = lis
     # print(fit)
 
     predict.recursive(fit, new_data, type = type, opts = opts, ...)
+}
+
+#' @export
+predict_recursive_panel_model_fit <- function(object, new_data, type = NULL, opts = list(), ...) {
+
+    # SETUP ----
+    y_var <- object$spec$y_var
+
+    if (is.null(y_var)) {
+        y_var <- object$preproc$y_var
+    }
+
+    pred_fun <- parsnip::predict.model_fit
+    .transform <- object$spec[["transform"]]
+    train_tail <- object$spec$train_tail
+    id <- object$spec$id
+
+    .id <- dplyr::ensym(id)
+
+    # print({
+    #     list(
+    #         object,
+    #         y_var,
+    #         class(object),
+    #         new_data,
+    #         train_tail
+    #     )
+    # })
+
+
+    # LOOP LOGIC ----
+    .preds <- tibble::tibble(.id = new_data %>% dplyr::select(!! .id) %>% purrr::as_vector(),
+                             .pred = numeric(nrow(new_data))) %>%
+        dplyr::group_by(.id) %>%
+        dplyr::mutate(rowid = dplyr::row_number()) %>%
+        dplyr::ungroup()
+
+    new_data <- new_data %>%
+        dplyr::group_by(!! .id) %>%
+        dplyr::mutate(rowid = dplyr::row_number()) %>%
+        dplyr::ungroup()
+
+    .first_slice <- new_data %>%
+        dplyr::group_by(!! .id) %>%
+        dplyr::slice_head(n = 1) %>%
+        dplyr::ungroup()
+
+
+    .preds[.preds$rowid == 1, 2] <- new_data[new_data$rowid == 1, y_var] <- pred_fun(object,
+                                                                                     new_data = .first_slice,
+                                                                                     type = type,
+                                                                                     opts = opts,
+                                                                                     ...)
+
+    .groups <- new_data %>%
+        dplyr::group_by(!! .id) %>%
+        dplyr::count(!! .id) %>%
+        dim() %>%
+        .[1]
+
+    new_data_size <- nrow(.preds)/.groups
+
+    for (i in 2:new_data_size) {
+
+        .temp_new_data <- dplyr::bind_rows(train_tail, new_data)
+
+        .nth_slice <- .transform(.temp_new_data, new_data_size, i, id)
+
+        .preds[.preds$rowid == i, 2] <- new_data[new_data$rowid == i, y_var] <- pred_fun(object,
+                                                                                         new_data = .nth_slice,
+                                                                                         type = type,
+                                                                                         opts = opts,
+                                                                                         ...)
+    }
+
+    return(.preds[,2])
+
+}
+
+#' @export
+predict_recursive_panel_workflow <- function(object, new_data, type = NULL, opts = list(), ...) {
+    workflow <- object
+
+    if (!workflow$trained) {
+        rlang::abort("Workflow has not yet been trained. Do you need to call `fit()`?")
+    }
+
+    blueprint <- workflow$pre$mold$blueprint
+    forged    <- hardhat::forge(new_data, blueprint)
+    new_data  <- forged$predictors
+
+    fit <- workflow$fit$fit
+
+    # print(fit)
+
+    predict.recursive_panel(fit, new_data, type = type, opts = opts, ...)
+}
+
+
+# HELPERS ----
+
+.prepare_panel_transform <- function(.transform) {
+
+    if (inherits(.transform, "function")) {
+
+        .transform_fun <- function(temp_new_data, new_data_size, slice_idx, id) {
+
+            id <- as.character(id)
+            id <- dplyr::ensym(id)
+
+            .transform(temp_new_data) %>%
+                dplyr::group_by(!! id) %>%
+                dplyr::group_split() %>%
+                purrr::map(function(x){
+
+                    dplyr::slice_tail(x, n = new_data_size) %>%
+                        .[slice_idx, ]
+
+                }) %>%
+                dplyr::bind_rows()
+        }
+    }
+
+    .transform_fun
+}
+
+#' @export
+panel_tail <- function(train_data, id, n){
+
+    id <- dplyr::ensym(id)
+
+    train_data <- train_data %>%
+        dplyr::group_by(!! id) %>%
+        dplyr::group_split() %>%
+        purrr::map(~tail(.x, n = n)) %>%
+        dplyr::bind_rows()
+
+    return(train_data)
+
 }
 
 
