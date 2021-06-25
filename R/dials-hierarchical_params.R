fb2fe9b707a82810d4db8fbae76ce3fd7d6328bd
Alberto Almuiña <albertogonzalezalmuinha@gmail.com>
2021-06-25 19:38:56 +0200

    Added Hierarchical Params

---
+++ R/dials-hierarchical_params.R
@@ -0,0 +1,41 @@
+#' Tuning Parameters for TEMPORAL HIERARCHICAL Models
+#'
+#'
+#' @inheritParams dials::Laplace
+#'
+#' @details
+#' The main parameters for Temporal Hierarchical models are:
+#'
+#'  - `combination_method`: Combination method of temporal hierarchies.
+#'  - `use_model`: Model used for forecasting each aggregation level.
+#'
+#' @examples
+#' combination_method()
+#'
+#' use_model()
+#'
+#' @name hierarchical_params
+
+
+#' @export
+#' @rdname hierarchical_params
+combination_method <- function() {
+    dials::new_qual_param(
+        type      = "character",
+        values    = c("struc", "mse", "ols", "bu", "shr", "sam"),
+        label     = c(method = "Combination method of temporal hierarchies."),
+        finalize  = NULL
+    )
+}
+
+
+#' @export
+#' @rdname hierarchical_params
+use_model <- function() {
+    dials::new_qual_param(
+        type      = "character",
+        values    = c("ets", "arima", "theta", "naive", "snaive"),
+        label     = c(method = "Model used for forecasting each aggregation level."),
+        finalize  = NULL
+    )
+}
