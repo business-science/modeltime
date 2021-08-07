6805f106f3e74ef08746cf476fcd8fd40a093439
Matt Dancho <mdancho@gmail.com>
2021-08-07 10:01:20 -0400

    nested_modeltime_refit() - initial commit

--- R/nested_modeltime_refit.R
+++ R/nested_modeltime_refit.R
@@ -1,29 +1,24 @@
 
+# NESTED REFIT ----
 
-#' Fit Tidymodels Workflows to Nested Time Series
+#' Refits a Nested Modeltime Table
 #'
 #' @description
-#' Fits one or more `tidymodels` workflow objects to nested time series data using the following process:
+#' Refits a Nested Modeltime Table to actual data using the following process:
 #'
-#' 1. Models are iteratively fit to training splits.
-#' 2. Accuracy is calculated on testing splits and is logged.
-#'   Accuracy results can be retrieved with [extract_nested_test_accuracy()]
-#' 3. Any model that returns an error is logged.
-#'   Error logs can be retrieved with [extract_nested_error_report()]
-#' 4. Forecast is predicted on testing splits and is logged.
-#'   Forecast results can be retrieved with [extract_nested_test_forecast()]
+#' 1. Models are iteratively refit to .actual_data.
+#' 2. Any model that returns an error is logged.
+#'  Errors can be retrieved with [extract_nested_error_report()]
+#' 3. Forecast is predicted on future_data and is logged.
+#'  Forecast can be retrieved with [extract_nested_future_forecast()]
 #'
 #' @inheritParams modeltime_accuracy
 #' @inheritParams modeltime_forecast
-#' @param nested_data Nested time series data
-#' @param ... Tidymodels `workflow` objects that will be fit to the nested time series data.
-#' @param control Used to control verbosity and parallel processing. See [control_nested_fit()].
+#' @param object A Nested Modeltime Table
+#' @param control Used to control verbosity and parallel processing. See [control_nested_refit()].
 #'
 #' @export
-modeltime_nested_fit <- function(nested_data, ...,
-                                 metric_set = default_forecast_accuracy_metric_set(),
-                                 conf_interval = 0.95,
-                                 control = control_nested_fit()) {
+modeltime_nested_refit <- function(object, conf_interval = 0.95, control = control_nested_refit()) {
 
     t1 <- Sys.time()
 
@@ -35,44 +30,52 @@ modeltime_nested_fit <- function(nested_data, ...,
 
     # HANDLE INPUTS ----
 
-    nested_data <- nested_data %>%
-        dplyr::select(1, ".actual_data", ".future_data", ".splits")
+    id_text <- attr(object, "id")
 
-    id_text <- names(nested_data)[[1]]
+    object <- object %>%
+        dplyr::select(dplyr::one_of(id_text), ".actual_data", ".future_data", ".splits", ".modeltime_tables")
 
     id_expr <- rlang::sym(id_text)
 
-    n_ids   <- nrow(nested_data)
+    n_ids   <- nrow(object)
 
-    x_expr  <- rlang::sym(".splits")
+    x_expr <- rlang::sym(".modeltime_tables")
 
-    d_expr  <- rlang::sym(".actual_data")
+    d_expr <- rlang::sym(".actual_data")
+
+    f_expr <- rlang::sym(".future_data")
 
 
     # SETUP LOGGING ENV ----
     logging_env <- rlang::env(
-        acc_tbl   = tibble::tibble(),
-        fcast_tbl = tibble::tibble(),
-        error_tbl = tibble::tibble()
+        fcast_tbl = tibble(),
+        error_tbl = tibble()
 
     )
 
     # SETUP PROGRESS
 
-    if (!control$verbose) cli::cli_progress_bar("Fitting models on training data...", total = nrow(nested_data), .envir = logging_env)
+    if (!control$verbose) cli::cli_progress_bar("Fitting models on training data...", total = nrow(object), .envir = logging_env)
 
     # LOOP LOGIC ----
 
-    nested_modeltime <- nested_data %>%
+    nested_modeltime <- object %>%
         tibble::rowid_to_column(var = '..rowid') %>%
         dplyr::mutate(
-            .modeltime_tables = purrr::pmap(.l = list(x = !! x_expr, d = !! d_expr, id = !! id_expr, i = ..rowid), .f = function(x, d, id, i) {
+            .modeltime_tables = purrr::pmap(.l = list(x = !! x_expr, d = !! d_expr, f = !! f_expr, id = !! id_expr, i = ..rowid), .f = function(x, d, f, id, i) {
+
+                # Save current model descriptions
+                # model_desc_user_vec          <- x$.model_desc
+                # model_desc_modeltime_old_vec <- x$.model %>% purrr::map_chr(get_model_description)
+
+                ..model_id <- x$.model_id
+
 
                 tryCatch({
 
                     if (control$verbose) cli::cli_alert_info(stringr::str_glue("[{i}/{n_ids}] Starting Modeltime Table: ID {id}..."))
 
-                    model_list <- list(...)
+                    model_list <- x$.model
 
                     safe_fit <- purrr::safely(mdl_time_refit, otherwise = NULL, quiet = TRUE)
 
@@ -82,7 +85,7 @@ modeltime_nested_fit <- function(nested_data, ...,
 
                             suppressMessages({
                                 suppressWarnings({
-                                    fit_list <- safe_fit(mod, data = rsample::training(x))
+                                    fit_list <- safe_fit(mod, data = d)
                                 })
                             })
 
@@ -92,7 +95,7 @@ modeltime_nested_fit <- function(nested_data, ...,
 
                             error_tbl <- tibble::tibble(
                                 !! id_text := id,
-                                .model_id   = i,
+                                .model_id   = ..model_id,
                                 .model_desc = get_model_description(mod),
                                 .error_desc = ifelse(is.null(err), NA_character_, err)
                             )
@@ -115,36 +118,46 @@ modeltime_nested_fit <- function(nested_data, ...,
                     ret <- tibble::tibble(
                         .model = .l
                     ) %>%
-                        tibble::rowid_to_column(var = ".model_id") %>%
+                        dplyr::mutate(.model_id = ..model_id) %>%
                         dplyr::mutate(.model_desc = purrr::map_chr(.model, .f = get_model_description)) %>%
 
                         # Simplify Naming
                         dplyr::mutate(.model_desc = gsub("[[:punct:][:digit:][:cntrl:]]", "", .model_desc)) %>%
                         dplyr::mutate(.model_desc = gsub(" WITH.*$", "", .model_desc))
 
+                    # Add calibration
+                    ret <- ret %>%
+                        dplyr::bind_cols(x[c(".type", ".calibration_data")])
+
+                    # Update class
                     class(ret) <- c("mdl_time_tbl", class(ret))
 
-                    # Calibration ----
-                    suppressMessages({
-                        suppressWarnings({
-                            ret <- ret %>%
-                                modeltime_calibrate(rsample::testing(x))
-                        })
-                    })
+                    # ret <- ret %>%
+                    #     dplyr::mutate(.model_desc_user = model_desc_user_vec) %>%
+                    #     dplyr::mutate(.model_desc_old  = model_desc_modeltime_old_vec) %>%
+                    #     dplyr::mutate(.model_desc_new  = purrr::map_chr(.model, .f = get_model_description)) %>%
+                    #
+                    #     # Description Logic
+                    #     dplyr::mutate(.model_desc = ifelse(
+                    #         .model_desc_old == .model_desc_new,
+                    #         # TRUE - Let User Choice Alone
+                    #         .model_desc_user,
+                    #         # FALSE - Model Algorithm Parameters Have Changed
+                    #         # - Reflect Updated Model Params in Description
+                    #         paste0("UPDATE: ", .model_desc_new)
+                    #         )
+                    #     ) %>%
+                    #
+                    #     # Clean up columns
+                    #     dplyr::select(-.model_desc_user, -.model_desc_old, -.model_desc_new)
 
+                    # Future Forecast ----
 
-                    # Accuracy ----
-                    acc_tbl <- modeltime_accuracy(ret, metric_set = metric_set) %>%
-                        tibble::add_column(!! id_text := id, .before = 1)
-
-                    logging_env$acc_tbl <- dplyr::bind_rows(logging_env$acc_tbl, acc_tbl)
-
-                    # Test Forecast ----
                     suppressMessages({
                         suppressWarnings({
                             fcast_tbl <- modeltime_forecast(
                                 object        = ret,
-                                new_data      = rsample::testing(x),
+                                new_data      = f,
                                 actual_data   = d,
                                 conf_interval = conf_interval
                             ) %>%
@@ -199,13 +212,13 @@ modeltime_nested_fit <- function(nested_data, ...,
 
     class(nested_modeltime) <- c("nested_mdl_time", class(nested_modeltime))
 
-    attr(nested_modeltime, "id")                  <- id_text
+    # attr(nested_modeltime, "id")                  <- id_text
     attr(nested_modeltime, "error_tbl")           <- logging_env$error_tbl %>% tidyr::drop_na()
-    attr(nested_modeltime, "accuracy_tbl")        <- logging_env$acc_tbl
-    attr(nested_modeltime, "test_forecast_tbl")   <- logging_env$fcast_tbl
-    attr(nested_modeltime, "best_selection_tbl")  <- NULL
-    attr(nested_modeltime, "future_forecast_tbl") <- NULL
-    attr(nested_modeltime, "fit_column")          <- ".splits"
+    # attr(nested_modeltime, "accuracy_tbl")        <- logging_env$acc_tbl
+    # attr(nested_modeltime, "test_forecast_tbl")   <- logging_env$fcast_tbl
+    # attr(nested_modeltime, "best_selection_tbl")  <- NULL
+    attr(nested_modeltime, "future_forecast_tbl") <- logging_env$fcast_tbl
+    attr(nested_modeltime, "fit_column")          <- ".actual_data"
     attr(nested_modeltime, "time_elapsed")        <- time_elapsed
 
 
@@ -218,27 +231,9 @@ modeltime_nested_fit <- function(nested_data, ...,
 
 }
 
-#' @export
-print.nested_mdl_time <- function(x, ...) {
-
-    # Collect inputs
-    fit_col <- attr(x, 'fit_column')
-    n_models_with_errors <- attr(x, "error_tbl") %>%
-        dplyr::pull(.model_id) %>%
-        unique() %>%
-        length()
-
-    cat("# Nested Modeltime Table\n")
-    cat("  ")
-    cli::cli_text(cli::col_grey("Trained on: {fit_col} | Model Errors: [{n_models_with_errors}]"))
-    # cli::cli_rule()
-    class(x) <- class(x)[!(class(x) %in% c("nested_mdl_time"))]
-    print(x, ...)
-}
-
 # CONTROL ----
 
-#' Control aspects of the `modeltime_nested_fit()` process.
+#' Control aspects of the `modeltime_nested_refit()` process.
 #'
 #' @param allow_par Logical to allow parallel computation. Default: `FALSE` (single threaded).
 #' @param cores Number of cores for computation. If -1, uses all available physical cores.
@@ -257,83 +252,15 @@ print.nested_mdl_time <- function(x, ...) {
 #'
 #'
 #' @seealso
-#' [modeltime_nested_fit()]
+#' [modeltime_nested_refit()]
 #'
 #' @examples
 #'
 #' # No parallel processing
-#' control_nested_fit()
+#' control_nested_refit()
 #'
 #' # With parallel processing
-#' control_nested_fit(allow_par = TRUE)
+#' control_nested_refit(allow_par = TRUE)
 #'
 #' @export
-control_nested_fit <- function(verbose = FALSE,
-                               allow_par = FALSE,
-                               cores = -1,
-                               packages = NULL) {
-
-    ret <- control_modeltime_objects(
-        verbose   = verbose,
-        allow_par = allow_par,
-        cores     = cores,
-        packages  = packages,
-        func      = "control_refit"
-    )
-
-    class(ret) <- c("control_nested_fit")
-
-    return(ret)
-}
-
-
-
-#' @export
-print.control_nested_fit <- function(x, ...) {
-    pretty_print_list(x, header = "nested fit control object")
-    invisible(x)
-}
-
-
-
-# NESTED TEST ACCURACY / TEST FORECAST / ERROR REPORTING ----
-
-#' Log Extractor Functions for Modeltime Nested Tables
-#'
-#' @description
-#' Extract logged information calculated during the `modeltime_nested_fit()`,
-#' `modeltime_nested_select_best()`, and `modeltime_nested_refit()` processes.
-#'
-#' @param object A nested modeltime table
-#'
-#' @name log_extractors
-
-#' @export
-#' @rdname log_extractors
-extract_nested_test_accuracy <- function(object) {
-    attr(object, "accuracy_tbl")
-}
-
-#' @export
-#' @rdname log_extractors
-extract_nested_test_forecast <- function(object) {
-    attr(object, "test_forecast_tbl")
-}
-
-#' @export
-#' @rdname log_extractors
-extract_nested_error_report <- function(object) {
-    attr(object, "error_tbl")
-}
-
-#' @export
-#' @rdname log_extractors
-extract_nested_best_model_report <- function(object) {
-    attr(object, "best_selection_tbl")
-}
-
-#' @export
-#' @rdname log_extractors
-extract_nested_future_forecast <- function(object) {
-    attr(object, "future_forecast_tbl")
-}
+control_nested_refit <- control_nested_fit
